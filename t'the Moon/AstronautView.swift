cbe5583803a9ae17f0df1d0ccfaad0df7f070793
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-09 14:22:04 -0400

    Add AstronautView

---
+++ t'the Moon/AstronautView.swift
@@ -0,0 +1,35 @@
+//
+//  AstronautView.swift
+//  t'the Moon
+//
+//  Created by Parth Antala on 2024-07-09.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+        var body: some View {
+            ScrollView {
+                VStack {
+                    Image(astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+
+                    Text(astronaut.description)
+                        .padding()
+                }
+            }
+            .background(.darkBackground)
+            .navigationTitle(astronaut.name)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+        return AstronautView(astronaut: astronauts["aldrin"]!)
+            .preferredColorScheme(.dark)
+}
