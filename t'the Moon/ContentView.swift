e7cebaeebc395697ab75b3148442e6dce78a39e2
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-09 19:39:43 -0400

    Add ListView toggle in Homescreen

--- t'the Moon/ContentView.swift
+++ t'the Moon/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    
+    @State private var isListView = false
     
     let columns = [
         GridItem(.adaptive(minimum: 150, maximum: 200))
@@ -21,56 +21,102 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                
-                LazyVGrid(columns: columns) {
+            ZStack {
+                List {
                     ForEach(missions) { mission in
                         NavigationLink {
                             MissionView(mission: mission, astronauts: astronauts)
                         } label: {
-                            VStack {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(.white)
-                                        .opacity(0.4)
-                                        .shadow(radius: 10)
-                              
-                                    
-                                    VStack {
-                                        Image(mission.image)
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: 100, height: 100)
+                            HStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80, height: 80)
+                                    .shadow(radius: 10)
+                                    .padding()
+                                
+                                VStack(alignment: .leading) {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white)
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                .opacity(isListView ? 1 : 0)
+                
+                ScrollView {
+                    
+                    LazyVGrid(columns: columns) {
+                        ForEach(missions) { mission in
+                            NavigationLink {
+                                MissionView(mission: mission, astronauts: astronauts)
+                            } label: {
+                                VStack {
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .fill(.white)
+                                            .opacity(0.4)
                                             .shadow(radius: 10)
-                                            .padding()
+                                        
                                         
                                         VStack {
-                                            Text(mission.displayName)
-                                                .font(.headline)
-                                                .foregroundStyle(.white)
-                                            Text(mission.formattedLaunchDate)
-                                                .font(.caption)
-                                                .foregroundStyle(.white)
+                                            Image(mission.image)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 100, height: 100)
+                                                .shadow(radius: 10)
+                                                .padding()
+                                            
+                                            VStack {
+                                                Text(mission.displayName)
+                                                    .font(.headline)
+                                                    .foregroundStyle(.white)
+                                                Text(mission.formattedLaunchDate)
+                                                    .font(.caption)
+                                                    .foregroundStyle(.white)
+                                            }
+                                            .padding(.vertical)
+                                            .frame(maxWidth: .infinity)
+                                            .background(.white.opacity(0.3))
                                         }
-                                        .padding(.vertical)
-                                        .frame(maxWidth: .infinity)
-                                        .background(.white.opacity(0.3))
                                     }
-                            }
-                            }
-                            .clipShape(.rect(cornerRadius: 10))
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
+                                }
+                                .clipShape(.rect(cornerRadius: 10))
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(.lightBackground)
+                                }
                             }
                         }
                     }
+                    .padding([.horizontal, .bottom])
                 }
-                .padding([.horizontal, .bottom])
+                .opacity(isListView ? 0 : 1)
+                
             }
             .navigationTitle("Missions")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                Button {
+                    withAnimation {
+                        isListView.toggle()
+                    }
+                } label: {
+                    if isListView {
+                        return Image(systemName: "square.grid.2x2")
+                    } else {
+                        return Image(systemName: "checklist.unchecked")
+                    }
+                }
+                .tint(.white)
+            }
         }
     }
 }
