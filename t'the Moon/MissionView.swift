b8b6617e1539e84351c40bbe714143074e6472c2
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-09 13:52:36 -0400

    Show mission details with ScrollView and containerRelativeFrame()

---
+++ t'the Moon/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  t'the Moon
+//
+//  Created by Parth Antala on 2024-07-09.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
